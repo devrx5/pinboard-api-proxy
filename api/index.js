3a2866cdd67981d14a2c0c2aa0cdc6fc730a56e6
devrx5 <71114231+devrx5@users.noreply.github.com>
2023-07-19 05:42:34 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ import { createProxyMiddleware } from 'http-proxy-middleware';
 import NextCors  from 'nextjs-cors';
 
 const apiProxy = createProxyMiddleware({
-  target: "https://v1.scgf.ru",
+  target: "https://sg-6.ru",
   changeOrigin: true,
   pathRewrite: {
     "^/api": "" // Strip "/api" from the URL 
